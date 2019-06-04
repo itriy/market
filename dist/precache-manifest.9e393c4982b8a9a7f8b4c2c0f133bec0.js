e590be036471c12b9c92d214387e4a27088ffd07
Aleksandr <itriy1@gmail.com>
2019-06-04 12:01:57 +0300

    add dist

---
+++ dist/precache-manifest.9e393c4982b8a9a7f8b4c2c0f133bec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "31da31031a835c2e6969",
+    "url": "/js/chunk-vendors.ac1416ca.js"
+  },
+  {
+    "revision": "f33f827bcbe18fe09164",
+    "url": "/js/app.f0c8d0b2.js"
+  },
+  {
+    "revision": "0485ffcd0db17bf755e9",
+    "url": "/js/about.49570efc.js"
+  },
+  {
+    "revision": "09cf43b240759247847ab04bd3cf6dc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f33f827bcbe18fe09164",
+    "url": "/css/app.84fe17e1.css"
+  }
+];
